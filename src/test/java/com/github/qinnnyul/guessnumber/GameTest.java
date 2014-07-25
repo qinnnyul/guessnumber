337a3749bde0026cea388f8bf719d16d88235252
qinnnyul <yulin.qin.ext@gmail.com>
2014-07-25 20:07:25 +0800

    Add more test cases to pass test coverage

--- src/test/java/com/github/qinnnyul/guessnumber/GameTest.java
+++ src/test/java/com/github/qinnnyul/guessnumber/GameTest.java
@@ -44,4 +44,52 @@ public class GameTest
         //then
         assertThat(result, is("4A0B"));
     }
+
+    /*
+    4 3 2 1    0A4B   4个数字位置都不对
+     */
+
+    @Test
+    public void should_be_able_to_return_0A2B_if_two_or_four_are_incorrect()
+    {
+        //given
+        Answer answer = Answer.createAnswer("2 4 7 8");
+        Game game = new Game(actualAnswer);
+
+        //when
+        String result = game.guess(answer);
+
+        //then
+        assertThat(result, is("0A2B"));
+    }
+
+    @Test
+    public void should_be_able_to_return_1A2B_if_four_is_right_two_and_three_are_incorrect()
+    {
+        //given
+        Answer answer = Answer.createAnswer("0 3 2 4");
+        Game game = new Game(actualAnswer);
+
+        //when
+        String result = game.guess(answer);
+
+        //then
+        assertThat(result, is("1A2B"));
+    }
+
+
+    @Test
+    public void should_be_able_to_return_0A4B_if_all_are_in_wrong_place()
+    {
+        //given
+        Answer answer = Answer.createAnswer("4 3 2 1");
+        Game game = new Game(actualAnswer);
+
+        //when
+        String result = game.guess(answer);
+
+        //then
+        assertThat(result, is("0A4B"));
+    }
+
 }
